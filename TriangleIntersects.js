22d618d8e390f7ab5a03c5ff48bb08fb5835ec95
Definitelynotaspruce <justjolantux@gmail.com>
2020-05-31 17:24:51 +0300

    Refactoring

--- TriangleIntersects.js
+++ TriangleIntersects.js
@@ -205,7 +205,7 @@ function tri_tri_intersect_with_isectline(V0, V1, V2, U0, U1, U2) {
     let E1 = [0.0, 0.0, 0.0], E2 = [0.0, 0.0, 0.0];
     let N1 = [0.0, 0.0, 0.0], N2 = [0.0, 0.0, 0.0], D = [0.0, 0.0, 0.0];
     let isect1 = [0.0, 0.0], isect2 = [0.0, 0.0];
-    let isectpointA1 = [0.0, 0.0, 0.0], isectpointA2 = [0.0, 0.0, 0.0]; 
+    let isectpointA1 = [0.0, 0.0, 0.0], isectpointA2 = [0.0, 0.0, 0.0];
     let isectpointB1 = [0.0, 0.0, 0.0], isectpointB2 = [0.0, 0.0, 0.0];
     let du0du1 = du0du2 = dv0dv1 = dv0dv2 = 0.0;
     let d1 = d2 = du0 = du1 = du2 = dv0 = dv1 = dv2 = 0.0;
@@ -269,7 +269,7 @@ function tri_tri_intersect_with_isectline(V0, V1, V2, U0, U1, U2) {
 
     // Projection onto athe axis correspoding to index
     // This corresponds to projection onto x, y, or z, whichver is closer to the direction of isectline D
-    vp0 = V0[index], vp1 = V0[index], vp2 = V2[index]; 
+    vp0 = V0[index], vp1 = V0[index], vp2 = V2[index];
     up0 = U0[index], up1 = U1[index], up2 = U2[index];
 
     let isect_first, isect_second, resArr;
@@ -304,14 +304,63 @@ function tri_tri_intersect_with_isectline(V0, V1, V2, U0, U1, U2) {
     return true;
 }
 
-function output_object_file(filename)
-{
-    fs.writeFile(filename, 'Hello content!', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
+function triangle_intersection_test(vertices) {
+    return tri_tri_intersect_with_isectline(vertices[0], vertices[1], vertices[2], vertices[3], vertices[4], vertices[5]);
 }
-// Nesikerta
+
+
+function output_object_file(filename, vertices, RGB) {
+    const fs = require('fs');
+    let entryData = "OFF\n\n6 2 6\n";
+    let triangleConnections = ["3 0 1 2", "3 3 4 5"];
+
+    let logStream = fs.createWriteStream(filename, { flags: 'w' });
+    logStream.write(entryData)
+    vertices.forEach(element => {
+        verticeInfo = element[0] + " " + element[1] + " " + element[2] + "\n";
+        logStream.write(verticeInfo);
+    });
+
+    let triangleConnection = "\n";
+    let i = 0;
+
+    RGB.forEach(triangleColor => {
+        triangleConnection += triangleConnections[i];
+        triangleColor.forEach(RGBComponent => {
+            triangleConnection = triangleConnection + " " + RGBComponent;
+        });
+        ++i;
+        triangleConnection += "\n";
+    });
+    logStream.write(triangleConnection);
+}
+
+function read_vertices_from_file(filename, filename_output, RGB) {
+    const readline = require('readline');
+    const fs = require('fs');
+
+    let rl = readline.createInterface({
+        input: fs.createReadStream(filename)
+    });
+
+    let vertices = [];
+    rl.on('line', (line) => {
+        vertice = line.split(" ");
+        vertices.push(vertice.map(Number));
+    }).on('close', () => {
+        if (vertices.length != 6)
+            throw 'You have to specify 6 vertices!';
+        console.log("Outputting to", filename_output, "\n");
+        output_object_file(filename_output, vertices, RGB);
+        console.log("Intersection", triangle_intersection_test(vertices) ? "occurs" : "does not occur");
+
+        return vertices;
+    });
+}
+
+
+// Tests
+/* // Nesikerta
 let V0 = [1.102, 7.440, 5.820];
 let V1 = [8.507, 9.527, 6.819];
 let V3 = [0.233, 3.748, 2.091];
@@ -390,36 +439,50 @@ console.log(tri_tri_intersect_with_isectline(V0, V1, V2, U1, U2, U3));
 console.log("Test true")
 V0 = [6.087, 4.020, 1.830];
 V1 = [6.921, 3.012, 3.967];
-V3 = [6.818 ,9.713, 4.930];
-U1 = [5.352 ,4.033, 5.671];
+V3 = [6.818, 9.713, 4.930];
+U1 = [5.352, 4.033, 5.671];
 U2 = [1.547, 8.536, 7.705];
-U3 = [7.832 ,3.273, 0.601];
+U3 = [7.832, 3.273, 0.601];
 console.log(tri_tri_intersect_with_isectline(V0, V1, V3, U1, U2, U3));
 V0 = [5.790, 9.870, 2.628];
 V1 = [6.572, 4.942, 1.053];
-V3 = [7.939 ,2.860, 1.922];
-U1 = [5.892 ,5.015, 4.031];
+V3 = [7.939, 2.860, 1.922];
+U1 = [5.892, 5.015, 4.031];
 U2 = [8.874, 9.951, 2.820];
-U3 = [0.054 ,5.089, 0.300];
+U3 = [0.054, 5.089, 0.300];
 console.log(tri_tri_intersect_with_isectline(V0, V1, V3, U1, U2, U3));
 V0 = [2.122, 6.273, 3.155];
 V1 = [7.787, 0.959, 6.768];
-V3 = [0.618 ,6.558, 0.444];
-U1 = [0.911 ,1.869, 1.445];
+V3 = [0.618, 6.558, 0.444];
+U1 = [0.911, 1.869, 1.445];
 U2 = [2.562, 6.024, 9.916];
-U3 = [4.275 ,3.684, 1.813];
+U3 = [4.275, 3.684, 1.813];
 console.log(tri_tri_intersect_with_isectline(V0, V1, V3, U1, U2, U3));
 V0 = [2.567, 6.464, 9.242];
 V1 = [3.936, 9.506, 1.128];
-V3 = [7.740 ,2.365, 2.224];
-U1 = [8.336 ,1.585, 2.203];
+V3 = [7.740, 2.365, 2.224];
+U1 = [8.336, 1.585, 2.203];
 U2 = [2.689, 4.811, 1.084];
-U3 = [6.166 ,3.953, 4.912];
+U3 = [6.166, 3.953, 4.912];
 console.log(tri_tri_intersect_with_isectline(V0, V1, V3, U1, U2, U3));
 V0 = [8.761, 9.205, 3.480];
 V1 = [4.238, 5.150, 7.019];
-V3 = [1.680 ,4.242, 0.266];
-U1 = [7.164 ,8.976, 6.716];
+V3 = [1.680, 4.242, 0.266];
+U1 = [7.164, 8.976, 6.716];
 U2 = [8.464, 1.031, 7.496];
-U3 = [0.182 ,4.119, 3.625];
-console.log(tri_tri_intersect_with_isectline(V0, V1, V3, U1, U2, U3));
+U3 = [0.182, 4.119, 3.625];
+console.log(tri_tri_intersect_with_isectline(V0, V1, V3, U1, U2, U3)); */
+
+let RGB = [[0, 0, 0], [255, 255, 0]];
+const inputFileName = "input.txt", outputFileName = "output.off";
+
+console.log("Reading vertices from input file...");
+try {
+    read_vertices_from_file(inputFileName, outputFileName, RGB);
+} catch (e) {
+    console.error(e);
+}
+
+
+
+
